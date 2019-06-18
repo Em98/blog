0f47228837298a055151254dc416f52fe6b1c1b3
yc <yc95227@gmail.com>
2019-06-18 14:04:50 +0800

    添加后台主页基础信息接口

--- src/main/java/com/yc/blogtemplate/controller/AdminController.java
+++ src/main/java/com/yc/blogtemplate/controller/AdminController.java
@@ -2,18 +2,25 @@ package com.yc.blogtemplate.controller;
 
 import com.yc.blogtemplate.domain.Post;
 import com.yc.blogtemplate.service.PostService;
+import com.yc.blogtemplate.util.ResponseUtil;
 import javafx.geometry.Pos;
+import org.omg.CORBA.OBJ_ADAPTER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
@@ -44,8 +51,20 @@ public class AdminController {
 
 
     @GetMapping("/index")
+
     public String toAdminIndex() {
         return "admin/adminIndex";
+    }
+
+    @GetMapping("/indexBasicInfo")
+    @ResponseBody
+    public Object getAdminIndexBasicInfo() {
+        Map<String, Object> data = new HashMap<>();
+        long postCount = postService.count();
+        LocalDateTime dateTime = LocalDateTime.now();
+        data.put("postCount", postCount);
+        data.put("dateTime", dateTime);
+        return ResponseUtil.ok(data);
     }
 
 }
